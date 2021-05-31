7a5e3ea6fd87bd565dbe53c0fba155c7d607dfc8
Md. Foysal <83161665+mdfoysal11@users.noreply.github.com>
2021-05-31 23:07:56 +0600

    added footer menu

--- footer.php
+++ footer.php
@@ -1,4 +1,19 @@
-
+<div class="container">
+	<div class="row">
+		<div class="col-md-6">
+			<p>&copy; All right revarsed</p>
+		</div>
+		<div class="col-md-6">
+			<?php 
+				wp_nav_menu(array(
+					"theme_location" => "footermenu",
+					"menu_id" => "footercontainer",
+					"menu_class" => "list-inline"
+				));
+			?>
+		</div>
+	</div>
+</div>
 <?php wp_footer()?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@ function alpha_bootstraping(){
     load_theme_textdomain( "alpha");
     add_theme_support( "title-tag");
     register_nav_menu( "topmenu", __("Top Menu", "alpha") );
+    register_nav_menu("footermenu", __("Footer Menu", "alpha"));
 
 }
 add_action( "after_setup_theme", "alpha_bootstraping");
